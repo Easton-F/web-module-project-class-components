b6bdb83a146319cefe9e8cacc97593149402d61c
Easton <easton.fritsch21@gmail.com>
2023-07-12 14:10:42 -0600

    built this.state and completed flags

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todos</h1>
         <ul>
-            <li>Walk the Dog</li>
-            <li>Take out Trash</li>
-            <li>Workout</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.name} { todo.completed?<span>- completed</span> : <span></span>}</li>)
+            })
+          }
         </ul>
 
         <form>
